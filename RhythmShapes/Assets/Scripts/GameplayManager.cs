519a253d3ce5f72639729b0bb385b10f3a398386
UsherMay <11yvon.00@gmail.com>
2022-10-15 23:09:13 +0200

    First Spawn Attempt

--- RhythmShapes/Assets/Scripts/GameplayManager.cs
+++ RhythmShapes/Assets/Scripts/GameplayManager.cs
@@ -15,8 +15,13 @@ public class GameplayManager : MonoBehaviour
     private Queue<Shape> _redShapeQueue;
     private Queue<Shape> _yellowShapeQueue;
     
+    //private Queue<float> _spawnTimes;
+    //private Queue<Shape> _colors;
     private AudioSource _audioSource;
     //private AudioClip _audioClip;
+    private float _globaleTime;
+    private float _goodWindow = 0.2f;
+    private float _tapTime;
     private void Awake()
     {
         Debug.Assert(Instance == null);
@@ -29,6 +34,11 @@ public class GameplayManager : MonoBehaviour
         _playerInputAction.Player.Red.performed += RedPerformed;
         _playerInputAction.Player.Yellow.performed += YellowPerformed;
         _audioSource = GetComponent<AudioSource>();
+
+        _blueShapeQueue = new Queue<Shape>();
+        _greenShapeQueue = new Queue<Shape>();
+        _redShapeQueue = new Queue<Shape>();
+        _yellowShapeQueue = new Queue<Shape>();
     }
     
     public void TestingPlayerInputTriggered(InputAction.CallbackContext context)
@@ -39,13 +49,48 @@ public class GameplayManager : MonoBehaviour
     public void Init(LevelDescription level)
     {
         // level.title
+        Debug.Log("levelTitle : " + level.title);
         // level.shapes
+        Shape shape;
+
+        for (int i = 0; i < level.shapes.Length; i++)
+        {
+            Debug.Log("iteration :" + i);
+            //_spawnTimes.Enqueue(level.shapes[i].timeToPress);
+            shape = ShapeFactory.Instance.GetShape(level.shapes[i].type);
+            shape.Init(level.shapes[i]);
+            Debug.Log(shape.TimeToSpawn);
+            
+            //Debug.Log(level.shapes[i].pathToFollow + ", length : " + level.shapes[i].pathToFollow.Length );
+
+            for (int j = 0; j < level.shapes[i].pathToFollow.Length ; j++)
+            {
+                //Debug.Log(level.shapes[i].pathToFollow[j]);
+            }
+            if (CompareColor(level.shapes[i].color, Color.blue))
+                _blueShapeQueue.Enqueue(shape);
+            else if (CompareColor(level.shapes[i].color, Color.red))
+                _redShapeQueue.Enqueue(shape);
+            else if (CompareColor(level.shapes[i].color, Color.green))
+                _greenShapeQueue.Enqueue(shape);
+            else if (CompareColor(level.shapes[i].color, Color.yellow))
+                _yellowShapeQueue.Enqueue(shape);
+            else
+            {
+                Debug.LogError("Unknown color, using blue as default");
+                _blueShapeQueue.Enqueue(shape);
+            }
+        }
         // shape.pathToFollow
         // ...
+        Debug.Log(_blueShapeQueue + ", " + _redShapeQueue + ", " +_greenShapeQueue +", " +_yellowShapeQueue);
+        
     }
 
     private void Update()
     {
+        _globaleTime += Time.deltaTime;
+        Debug.Log("_globalTime : " + _globaleTime);
         if (Input.GetKey(KeyCode.P) && !_audioSource.isPlaying)
         {
             StartMusic();
@@ -73,35 +118,90 @@ public class GameplayManager : MonoBehaviour
 
     private void BluePerformed(InputAction.CallbackContext context)
     {
-        Debug.Log("BluePerformed : " + context);
+        //Debug.Log("BluePerformed : " + context);
 
-        Shape currentShape = _blueShapeQueue.Peek();
-        if (currentShape.TimeToPress == _audioSource.time)
-        {
-            ShapeFactory.Instance.Release(_blueShapeQueue.Dequeue());
-        }
         
+        if (_blueShapeQueue.Count != 0)
+        {
+            Shape currentShape = _blueShapeQueue.Peek();
+            _tapTime = _audioSource.time;
+            if (_tapTime > currentShape.TimeToPress - _goodWindow || _tapTime < currentShape.TimeToPress + _goodWindow)
+            {
+                Debug.Log("Blue : GOOOOOOOD");
+                ShapeFactory.Instance.Release(_blueShapeQueue.Dequeue());
+            }
+            else
+            {
+                Debug.Log("Blue : FAILED");
+            }
+        }
+ 
     }
     
     private void GreenPerformed(InputAction.CallbackContext context)
     {
-        Debug.Log("GreenPerformed : " + context);
-        
-        Shape currentShape = _blueShapeQueue.Peek();
-        if (currentShape.TimeToPress == _audioSource.time)
+        //Debug.Log("GreenPerformed : " + context);
+        if (_greenShapeQueue.Count != 0)
         {
-            ShapeFactory.Instance.Release(_blueShapeQueue.Dequeue());
+            Shape currentShape = _greenShapeQueue.Peek();
+            _tapTime = _audioSource.time;
+            if (_tapTime > currentShape.TimeToPress - _goodWindow || _tapTime < currentShape.TimeToPress + _goodWindow)
+            {
+                Debug.Log("Green : GOOOOOOOD");
+                ShapeFactory.Instance.Release(_greenShapeQueue.Dequeue());
+            }
+            else
+            {
+                Debug.Log("Green : FAILED");
+            }
         }
+        
     }
     
     private void RedPerformed(InputAction.CallbackContext context)
     {
-        Debug.Log("RedPerformed : " + context);
+        //Debug.Log("RedPerformed : " + context);
+
+        if (_redShapeQueue.Count != 0)
+        {
+            Shape currentShape = _redShapeQueue.Peek();
+            _tapTime = _audioSource.time;
+            if (_tapTime > currentShape.TimeToPress - _goodWindow || _tapTime < currentShape.TimeToPress + _goodWindow)
+            {
+                Debug.Log("Red : GOOOOOOOD");
+                ShapeFactory.Instance.Release(_redShapeQueue.Dequeue());
+            }
+            else
+            {
+                Debug.Log("Red : FAILED");
+            }
+        }
     }
     
     private void YellowPerformed(InputAction.CallbackContext context)
     {
-        Debug.Log("YellowPerformed : " + context);
+        //Debug.Log("YellowPerformed : " + context);
+
+        if (_yellowShapeQueue.Count !=0)
+        {
+            Shape currentShape = _yellowShapeQueue.Peek();
+            _tapTime = _audioSource.time;
+            if (_tapTime > currentShape.TimeToPress - _goodWindow || _tapTime < currentShape.TimeToPress + _goodWindow)
+            {
+                Debug.Log("Yellow : GOOOOOOOD");
+                ShapeFactory.Instance.Release(_yellowShapeQueue.Dequeue());
+            }
+            else
+            {
+                Debug.Log("Yellow : FAILED");
+            }
+        }    
+        
+    }
+    
+    private bool CompareColor(Color32 c1, Color32 c2)
+    {
+        return c1.r == c2.r && c1.g == c2.g && c1.b == c2.b && c1.a == c2.a;
     }
     
 }
