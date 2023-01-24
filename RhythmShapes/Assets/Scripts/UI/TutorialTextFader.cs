0e73d9cc9d98b640b5ef37e0a93e891d91a13ac0
MartialHuot <martial.huot@telecom-sudparis.eu>
2023-01-24 18:36:59 +0100

    Tutorial text now fades out after some time.

--- RhythmShapes/Assets/Scripts/UI/TutorialTextFader.cs
+++ RhythmShapes/Assets/Scripts/UI/TutorialTextFader.cs
@@ -6,19 +6,28 @@ using TMPro;
 
 public class TutorialTextFader : MonoBehaviour
 {
+    [SerializeField] private GameObject regionName0;
     [SerializeField] private GameObject regionName1;
     [SerializeField] private GameObject regionName2;
     [SerializeField] private GameObject regionName3;
     [SerializeField] private GameObject regionName4;
     [SerializeField] private GameObject regionName5;
     [SerializeField] private GameObject tutorialTextCanvas;
-    [SerializeField] private float timing1;
-    [SerializeField] private float timing2;
-    [SerializeField] private float timing3;
-    [SerializeField] private float timing4;
-    [SerializeField] private float timing5;
+
+    [SerializeField] private float timeIn1;
+    [SerializeField] private float timeOut1;
+    [SerializeField] private float timeIn2;
+    [SerializeField] private float timeOut2;
+    [SerializeField] private float timeIn3;
+    [SerializeField] private float timeOut3;
+    [SerializeField] private float timeIn4;
+    [SerializeField] private float timeOut4;
+    [SerializeField] private float timeIn5;
+    [SerializeField] private float timeOut5;
+
     [SerializeField] private float timingEnd;
 
+    private TMP_Text text0;
     private TMP_Text text1;
     private TMP_Text text2;
     private TMP_Text text3;
@@ -30,31 +39,35 @@ public class TutorialTextFader : MonoBehaviour
 
     void Start()
     {
+        text0 = regionName0.GetComponent<TMP_Text>();
         text1 = regionName1.GetComponent<TMP_Text>();
         text2 = regionName2.GetComponent<TMP_Text>();
         text3 = regionName3.GetComponent<TMP_Text>();
         text4 = regionName4.GetComponent<TMP_Text>();
         text5 = regionName5.GetComponent<TMP_Text>();
 
+        text0.CrossFadeAlpha(0, 18f, false);
         text1.CrossFadeAlpha(0, 0.0f, false);
         text2.CrossFadeAlpha(0, 0.0f, false);
         text3.CrossFadeAlpha(0, 0.0f, false);
         text4.CrossFadeAlpha(0, 0.0f, false);
         text5.CrossFadeAlpha(0, 0.0f, false);
 
-        StartCoroutine(FadeIn(text1, timing1));
-        StartCoroutine(FadeIn(text2, timing2));
-        StartCoroutine(FadeIn(text3, timing3));
-        StartCoroutine(FadeIn(text4, timing4));
-        StartCoroutine(FadeIn(text5, timing5));
+        StartCoroutine(FadeIn(text1, timeIn1, timeOut1));
+        StartCoroutine(FadeIn(text2, timeIn2, timeOut2));
+        StartCoroutine(FadeIn(text3, timeIn3, timeOut3));
+        StartCoroutine(FadeIn(text4, timeIn4, timeOut4));
+        StartCoroutine(FadeIn(text5, timeIn5, timeOut5));
 
         StartCoroutine(EndTutorial(timingEnd));
     }
 
-    private IEnumerator FadeIn(TMP_Text textObject, float waitTime)
+    private IEnumerator FadeIn(TMP_Text textObject, float waitTime, float disappearTime)
     {
         yield return new WaitForSeconds(waitTime);
         textObject.CrossFadeAlpha(1, fadeTime, false);
+        yield return new WaitForSeconds(disappearTime);
+        textObject.CrossFadeAlpha(0, fadeTime, false);
     }
 
     private IEnumerator EndTutorial(float waitTime)
