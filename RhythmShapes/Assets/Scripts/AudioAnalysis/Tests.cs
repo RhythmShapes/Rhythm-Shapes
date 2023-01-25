fa8fae19a02ecf2c998016c3b7e2c2dcd0ed598d
Paul VANCAUWENBERGHE <paul.vancauwenberghe@telecom-sudparis.eu>
2023-01-25 20:25:39 +0100

    commented unimplemented functions

--- RhythmShapes/Assets/Scripts/AudioAnalysis/Tests.cs
+++ RhythmShapes/Assets/Scripts/AudioAnalysis/Tests.cs
@@ -41,15 +41,15 @@ public class Tests : MonoBehaviour
         Vector2[] path = { new Vector2(0, 0), new Vector2(1, 1) };
         Shape shape = new Shape();
         ShapeModel model = new ShapeModel(ShapeType.Square, Target.Top, leftTargetColor.color, path, 1f, 0.15f, 1f);
-        shape.SetModel(model);
+        //shape.SetModel(model);
         shapes.Add(shape);
         for (int i = 1; i < 52; i++)
         {
             float time = Random.Range(i * 60 / bpm, (i + 1) * 60 / bpm);
             shape = new Shape();
             model = new ShapeModel(ShapeType.Square, Target.Top, leftTargetColor.color, path, 1f, 0.15f, 1f);
-            shape.SetModel(model);
-            shape.SetTimeToPress(time);
+            //shape.SetModel(model);
+            //shape.SetTimeToPress(time);
             shapes.Add(shape);
         }
 
@@ -61,8 +61,8 @@ public class Tests : MonoBehaviour
     {
         float bpm = 120f;//Random.value * 100 + 100;
         Shape[] data = getSampleData(bpm);
-        data[0].SetTimeToPress(0.62134f);
-        data[5].SetTimeToPress(2.58f);
+        //data[0].SetTimeToPress(0.62134f);
+        //data[5].SetTimeToPress(2.58f);
         Debug.Log(data.Length);
         Debug.Log(data[0].TimeToPress);
         Debug.Log("5: "+data[5].TimeToPress);
