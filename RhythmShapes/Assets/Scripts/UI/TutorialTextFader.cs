410945911e122f9cbec354d5e7e2e927dcc6d9e9
MartialHuot <martial.huot@telecom-sudparis.eu>
2023-01-26 14:47:56 +0100

    Fixed text fades and scale

--- RhythmShapes/Assets/Scripts/UI/TutorialTextFader.cs
+++ RhythmShapes/Assets/Scripts/UI/TutorialTextFader.cs
@@ -14,6 +14,8 @@ public class TutorialTextFader : MonoBehaviour
     [SerializeField] private GameObject regionName5;
     [SerializeField] private GameObject tutorialTextCanvas;
 
+    [SerializeField] private float timeIn0;
+    [SerializeField] private float timeOut0;
     [SerializeField] private float timeIn1;
     [SerializeField] private float timeOut1;
     [SerializeField] private float timeIn2;
@@ -34,11 +36,15 @@ public class TutorialTextFader : MonoBehaviour
     private TMP_Text text4;
     private TMP_Text text5;
 
+    private AudioSource _audioSource;
+
     [SerializeField] private float fadeTime;
-    
+
 
     void Start()
     {
+        _audioSource = GetComponent<AudioSource>();
+
         text0 = regionName0.GetComponent<TMP_Text>();
         text1 = regionName1.GetComponent<TMP_Text>();
         text2 = regionName2.GetComponent<TMP_Text>();
@@ -46,33 +52,48 @@ public class TutorialTextFader : MonoBehaviour
         text4 = regionName4.GetComponent<TMP_Text>();
         text5 = regionName5.GetComponent<TMP_Text>();
 
-        text0.CrossFadeAlpha(0, 18f, false);
-        text1.CrossFadeAlpha(0, 0.0f, false);
-        text2.CrossFadeAlpha(0, 0.0f, false);
-        text3.CrossFadeAlpha(0, 0.0f, false);
-        text4.CrossFadeAlpha(0, 0.0f, false);
-        text5.CrossFadeAlpha(0, 0.0f, false);
+        ShowTexts();
+    }
 
-        StartCoroutine(FadeIn(text1, timeIn1, timeOut1));
-        StartCoroutine(FadeIn(text2, timeIn2, timeOut2));
-        StartCoroutine(FadeIn(text3, timeIn3, timeOut3));
-        StartCoroutine(FadeIn(text4, timeIn4, timeOut4));
-        StartCoroutine(FadeIn(text5, timeIn5, timeOut5));
+    void ShowTexts() 
+    {
+        tutorialTextCanvas.SetActive(true);
+        StartCoroutine(FadeIn(regionName0, text0, timeIn0, timeOut0));
+        StartCoroutine(FadeIn(regionName1, text1, timeIn1, timeOut1));
+        StartCoroutine(FadeIn(regionName2, text2, timeIn2, timeOut2));
+        StartCoroutine(FadeIn(regionName3, text3, timeIn3, timeOut3));
+        StartCoroutine(FadeIn(regionName4, text4, timeIn4, timeOut4));
+        StartCoroutine(FadeIn(regionName5, text5, timeIn5, timeOut5));
 
         StartCoroutine(EndTutorial(timingEnd));
     }
 
-    private IEnumerator FadeIn(TMP_Text textObject, float waitTime, float disappearTime)
+    private IEnumerator FadeIn(GameObject regionObject, TMP_Text textObject, float waitTime, float disappearTime)
     {
-        yield return new WaitForSeconds(waitTime);
+        yield return new WaitUntil(() => _audioSource.time >= waitTime);
+        textObject.CrossFadeAlpha(0, 0f, false);
+        regionObject.SetActive(true);
         textObject.CrossFadeAlpha(1, fadeTime, false);
-        yield return new WaitForSeconds(disappearTime);
+        yield return new WaitUntil(() => _audioSource.time >= disappearTime);
         textObject.CrossFadeAlpha(0, fadeTime, false);
+        yield return new WaitForSeconds(fadeTime);
+        regionObject.SetActive(false);
     }
 
     private IEnumerator EndTutorial(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
         tutorialTextCanvas.SetActive(false);
+    }
+
+    public void OnGameRestarted()
+    {
+        regionName0.SetActive(false);
+        regionName1.SetActive(false);
+        regionName2.SetActive(false);
+        regionName3.SetActive(false);
+        regionName4.SetActive(false);
+        regionName5.SetActive(false);
+        ShowTexts();
     }
 }
