9a9fb78d60aab58ba7ddb065a681ba6e2f67b502
UsherMay <11yvon.00@gmail.com>
2023-01-26 01:46:08 +0100

    SetupFonction if the folder Levels doesn't exist

--- RhythmShapes/Assets/Scripts/SetupDatapath.cs
+++ RhythmShapes/Assets/Scripts/SetupDatapath.cs
@@ -1,36 +1,24 @@
 using System.IO;
 using UnityEngine;
+using utils;
 
 public class SetupDatapath : MonoBehaviour
 {
     public bool setupNeeded = true;
     private string applicationPersistentDataPath;
-    private string applicationDataPath;
     private void Awake()
     {
         applicationPersistentDataPath = Application.persistentDataPath;
-        applicationDataPath = Application.dataPath;
-        if(Directory.Exists(Path.Combine(applicationPersistentDataPath,"Levels","LevelTest")))
+
+        //if(Directory.Exists(Path.Combine(applicationPersistentDataPath,"Levels","LevelTest")))
+        if(Directory.Exists(Path.Combine(applicationPersistentDataPath,"Levels")))
         {
             setupNeeded = false;
         }
 
         if (setupNeeded)
         {
-            File.Copy(Path.Combine(applicationDataPath,"Resources","Levels","LevelTest"), Path.Combine(applicationPersistentDataPath,"Levels","LevelTest"));
-        }
-    }
-    
-    public void SetUpCalibrationStage()
-    {
-        if(Directory.Exists(Path.Combine(applicationPersistentDataPath,"Levels","Calibration31sec")))
-        {
-            setupNeeded = false;
-        }
-
-        if (setupNeeded)
-        {
-            File.Copy(Path.Combine(applicationDataPath,"Resources","Levels","Calibration31sec"), Path.Combine(applicationPersistentDataPath,"Levels","Calibration31sec"));
+            Directory.CreateDirectory(Path.Combine(applicationPersistentDataPath, "Levels"));
         }
     }
 }
