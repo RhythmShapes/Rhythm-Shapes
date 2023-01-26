6e932eb5adc1a1339028e6250661832db58eb6a0
dozlo <dozlouka@gmail.com>
2023-01-26 19:11:37 +0100

    fix commit

---
+++ RhythmShapes/Assets/Scripts/UI/EditorMusicSelectionManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class EditorMusicSelectionManager : MusicSelectionManager
+{
+    public new static EditorMusicSelectionManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        Debug.Assert(Instance == null);
+        Instance = this;
+    }
+}
