7a2ef5e5c85c392d9c41c3e67ecc05c0621d12f5
UsherMay <11yvon.00@gmail.com>
2023-01-27 20:43:00 +0100

    Update MultiRangeAnalysis.cs

--- RhythmShapes/Assets/Scripts/AudioAnalysis/MultiRangeAnalysis.cs
+++ RhythmShapes/Assets/Scripts/AudioAnalysis/MultiRangeAnalysis.cs
@@ -262,12 +262,15 @@ namespace AudioAnalysis
             }
             Debug.Log("BPM = " + bpm);
             //LevelPreparator.TravelTime = 2* 60 / bpm;
-            PostAnalysisTools.RoundTimingToMilliseconds(finalShapes);
-            ProgressUtil.Update();
-            int fbi = PostAnalysisTools.FirstBeatIndex(finalShapes, bpm);
-            PostAnalysisTools.snapNotesToBPMGrid(finalShapes, bpm, finalShapes[fbi].timeToPress, 1f/ 12);
-            ProgressUtil.Update();
-            PostAnalysisTools.RythmicPatternRepetition(finalShapes, bpm, finalShapes[fbi].timeToPress, 0.91f);
+            if (finalShapes != null && finalShapes.Length > 0)
+            {
+                PostAnalysisTools.RoundTimingToMilliseconds(finalShapes);
+                ProgressUtil.Update();
+                int fbi = PostAnalysisTools.FirstBeatIndex(finalShapes, bpm);
+                PostAnalysisTools.snapNotesToBPMGrid(finalShapes, bpm, finalShapes[fbi].timeToPress, 1f / 12);
+                ProgressUtil.Update();
+                PostAnalysisTools.RythmicPatternRepetition(finalShapes, bpm, finalShapes[fbi].timeToPress, 0.91f);
+            }
             level.shapes = shapes.ToArray();
             ProgressUtil.Update();
             
